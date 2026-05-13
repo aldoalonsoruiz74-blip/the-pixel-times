fc40648cb5c54c084ecbbe34cbc39bc983af73cf
aldoalonsoruiz74-blip <aldoalonsoruiz74@gmail.com>
2026-05-12 18:09:09 -0600

    Delete noticiero-retro directory

--- noticiero-retro/js/config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-  newsJsonPath: 'data/news.json',
-  adminPassword: 'admin123',
-  localStorageKey: 'pixel_times_admin_news',
-  defaultCategory: 'todas',
-  debounceTime: 300,
-};
